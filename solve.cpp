d6509b57346b90ce54b7dd00e5769dc33278e0a2
Finn <currencyfinn@gmail.com>
2023-01-07 11:01:21 +0100

    possibility to use vector frames instead array

--- solve.cpp
+++ solve.cpp
@@ -12,13 +12,21 @@ using namespace std;
 
 //int checkArray[9] = {1,2,3,4,5,6,7,8,9};
 int frame[9][9] = { {4,8,1,7,6,9,3,5,0}, {5,6,0,3,2,4,0,7,0}, {3,0,0,1,5,0,6,0,0}, {0,9,7,1,0,0,5,4,0}, {0,8,5,0,0,6,1,0,0}, {2,1,0,5,4,0,0,8,6}, {0,7,6,2,0,5,9,3,4}, {9,5,3,0,4,0,0,0,0}, {0,2,0,0,3,0,0,6,5} };
-int hor_frame[9][9];
-int ver_frame[9][9];
+vector<vector<int>> vecFrame = { {4,8,1,7,6,9,3,5,0}, {5,6,0,3,2,4,0,7,0}, {3,0,0,1,5,0,6,0,0}, {0,9,7,1,0,0,5,4,0}, {0,8,5,0,0,6,1,0,0}, {2,1,0,5,4,0,0,8,6}, {0,7,6,2,0,5,9,3,4}, {9,5,3,0,4,0,0,0,0}, {0,2,0,0,3,0,0,6,5} };
+
+int horFrame[9][9];
+vector<vector<int>> horVector (9, vector<int>(9));
+int verFrame[9][9];
+vector<vector<int>> verVector (9, vector<int>(9));
+
 int checkArray[9] = {1,2,3,4,5,6,7,8,9};
+
 vector<vector<vector<int>>> solveFrame(9, vector<vector<int> > (9, vector<int>(9)));
 // int int_count =0;
 
 // internal block 
+
+
 
 void intSolveSetup(int inputMatrix[9][9])
 {
@@ -26,6 +34,28 @@ void intSolveSetup(int inputMatrix[9][9])
         for(int j = 0; j != solveFrame[i].size(); j++)
         {   
             if (inputMatrix[i][j] != 0) {
+                solveFrame[i][j].resize(1);
+                solveFrame[i][j][0] = 0;
+            }
+            else {
+
+                    for (int k = 0; k != solveFrame[i][j].size(); k++)
+                    {   
+                        solveFrame[i][j][k] = checkArray[k];
+                    }
+                
+            }
+         }
+    }
+    return;
+}
+
+void intSolveSetupVector(vector<vector<int>>& inputVector)
+{
+    for (int i=0; i<solveFrame.size(); i++) {
+        for(int j = 0; j != solveFrame[i].size(); j++)
+        {   
+            if (inputVector[i][j] != 0) {
                 solveFrame[i][j].resize(1);
                 solveFrame[i][j][0] = 0;
             }
@@ -71,9 +101,39 @@ void intSolve(int inputMatrix[9][9])
 }
 
 
+void intSolveVector(vector<vector<int>>& inputVector)
+{
+    for (int i =0; i<9; i++)
+    {
+        vector<int> tempVec;
+        for (int j=0; j<9; j++) {
+            tempVec.push_back(inputVector[i][j]);
+        }
+        for (int k=0; k<9; k++)
+        if (solveFrame[i][k][0] != 0) {
+            for (int l=0; l<9; l++) {
+                vector<int>::iterator it = find(solveFrame[i][k].begin(), solveFrame[i][k].end(), tempVec[l]); 
+                if (it != solveFrame[i][k].end()) {
+                    solveFrame[i][k].erase(it);
+                }
+                // else {
+                //     int_count++;
+                // }
+                if (solveFrame[i][k].size() == 1) {
+                    inputVector[i][k] = solveFrame[i][k][0];
+                    solveFrame[i][k][0] = 0;
+                }
+            }
+        }
+    }
+    return;
+}
+
+
+
 // horizonatal rows
 
-void hor_solve(int inputMatrix[9][9], int outputMatrix[9][9])
+void horSolve(int inputMatrix[9][9], int outputMatrix[9][9])
 {
     int acount = 0;
     int bcount = 0;
@@ -171,6 +231,105 @@ void hor_solve(int inputMatrix[9][9], int outputMatrix[9][9])
     return;
 }
 
+void vecHorSolve(vector<vector<int>>& inputVector, vector<vector<int>>& outputVector)
+{
+    int acount = 0;
+    int bcount = 0;
+    int ccount = 0;
+    for(int i = 0; i<inputVector.size(); i++)
+    {
+        if(i < 3) // first row block
+        {
+            for(int b = 0; b != inputVector[i].size(); b++) {          
+                if(b <3) // first segment row
+                {
+                    if (acount >8) {
+                        acount = 0;
+                    }
+                    outputVector[0][acount] = inputVector[i][b];
+                    acount++;
+                }
+                else if (b>2 && b<6) // second segment row
+                {
+                    if (bcount >8) {
+                        bcount = 0;
+                    }
+                    outputVector[1][bcount] = inputVector[i][b];
+                    bcount++;
+                } 
+                else // third segment row
+                {
+                    if (ccount > 8) {
+                        ccount = 0;
+                    }
+                    outputVector[2][ccount] =  inputVector[i][b];
+                    ccount++;
+                }
+            }
+        }  
+        else if (i>2 && i<6) // second row block
+        {
+            for(int b = 0; b!= inputVector[i].size(); b++) {
+                if(b <3) // first segment row
+                {
+                    if (acount >8) {
+                        acount = 0;
+                    }
+                    outputVector[3][acount] = inputVector[i][b];
+                    acount++;
+                }
+                else if (b>2 && b<6) // second segment row
+                {
+                    if (bcount >8) {
+                        bcount = 0;
+                    }
+                    outputVector[4][bcount] = inputVector[i][b];
+                    bcount++;
+                } 
+                else // third segment row
+                {
+                    if (ccount > 8) {
+                        ccount = 0;
+                    }
+                    outputVector[5][ccount] = inputVector[i][b];
+                    ccount++;
+                }
+            }
+        } 
+        else // third row block
+        {
+            for(int b = 0; b!= inputVector[i].size(); b++) {
+                if(b <3) // first segment row
+                {
+                    if (acount >8) {
+                        acount = 0;
+                    }
+                    outputVector[6][acount] = inputVector[i][b];
+                    acount++;
+                }
+                else if (b>2 && b<6) // second segment row
+                {
+                    if (bcount >8) {
+                        bcount = 0;
+                    }
+                    outputVector[7][bcount] = inputVector[i][b];
+                    bcount++;
+                } 
+                else // third segment row
+                {
+                    if (ccount > 8) {
+                        ccount = 0;
+                    }
+                    outputVector[8][ccount] = inputVector[i][b];
+                    ccount++;
+                }
+            }
+        }
+    }
+    return;
+}
+
+
 void transpose(int inputMatrix[9][9], int outputMatrix[9][9]) {
      for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
@@ -178,6 +337,15 @@ void transpose(int inputMatrix[9][9], int outputMatrix[9][9]) {
       }
     }
 }
+
+void transposeVector(vector<vector<int>>& inputVector, vector<vector<int>>& outputVector) {
+     for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            outputVector[j][i] = inputVector[i][j];
+      }
+    }
+}
+
 
 void visualise(int matrix[9][9]) {
     for (int i=0; i<9; i++) {
@@ -190,7 +358,19 @@ void visualise(int matrix[9][9]) {
     return;
 }
 
-void visualiseVec(vector<vector<vector<int>>>& vec) {
+
+void visualiseVec2D(vector<vector<int>>& vec) {
+    for (int i=0; i<vec.size(); i++) {
+        for(int j=0; j != vec[i].size(); j++){
+            cout << vec[i][j];
+         }
+         cout << ' ';
+    }
+    cout << endl;
+    return;
+}
+
+void visualiseVec3D(vector<vector<vector<int>>>& vec) {
     for (int i=0; i<vec.size(); i++) {
         for(int j=0; j != vec[i].size(); j++){
             for (int k = 0; k != vec[i][j].size(); k++)
@@ -243,24 +423,31 @@ int main()
     // while(int_count !=27) {
 
     // }
-    hor_solve(frame, hor_frame);
-    transpose(hor_frame, ver_frame);
-    intSolveSetup(hor_frame);
+    //horSolve(frame, horFrame);
+    //transpose(hor_frame, verFrame);
+    //intSolveSetup(horFrame);
+
+    vecHorSolve(vecFrame, horVector);
+    transposeVector(horVector, verVector);
+    intSolveSetupVector(vecFrame);
 
 
     // ## initial frame:
-    visualise(hor_frame);
+    //visualise(horFrame);
+    visualiseVec2D(horVector);
 
 
     // ## solving + visualising
-    intSolve(hor_frame);
+    //intSolve(horFrame);
     //visualiseVec(solveFrame);
-
+    intSolveVector(horVector);
 
     // ## finial frame:
-    visualise(hor_frame);
-    //hor_solve(frame, hor_frame);
+    //visualise(horFrame);
+    //hor_solve(frame, horFrame);
+    visualiseVec2D(horVector);
     
-    writeOutfile(hor_frame);
+    //writeOutfile(horFrame);
+
     return 0;
 }
